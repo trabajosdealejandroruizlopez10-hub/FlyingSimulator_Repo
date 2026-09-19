12ccc48a1f0232b1fb05eaa7f599d9dba9862892
Dakasta <diegpsanrracho@gmail.com>
2026-09-19 16:18:32 +0200

    flight-system-maincode

---
+++ FlyingSimulator_Project/Assets/Scripts/flightNavigation.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirplaneController : MonoBehaviour
+{
+    public float FlySpeed = 5;
+    public float YawAmount = 120;
+
+    private float Yaw;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // move forward
+        transform.position += transform.forward * FlySpeed * Time.deltaTime;
+
+        // inputs
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // yaw, pitch, roll
+        Yaw += horizontalInput * YawAmount * Time.deltaTime;
+        float pitch = Mathf.Lerp(0, 20, Mathf.Abs(verticalInput)) * Mathf.Sign(verticalInput);
+        float roll = Mathf.Lerp(0, 30, Mathf.Abs(horizontalInput)) * -Mathf.Sign(horizontalInput); 
+
+        // apply rotation
+        transform.rotation = Quaternion.Euler(Vector3.up * Yaw + Vector3.right * pitch + Vector3.forward * roll);
+    }
+}
